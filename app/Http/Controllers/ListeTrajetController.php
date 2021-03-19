4d78a86d15a8d4fad977a7c5aded2881e5f1c26d
vivianenyakou@gmail.com <your_email_address@example.com>
2021-03-19 21:49:01 +0000

    ok

--- app/Http/Controllers/ListeTrajetController.php
+++ app/Http/Controllers/ListeTrajetController.php
@@ -13,7 +13,7 @@ class ListeTrajetController extends Controller
      */
     public function index()
     {
-        //
+        return view('Personne.ListeTrajet');
     }
 
     /**
---
+++ resources/views/Personne/ListeTrajet.blade.php
@@ -0,0 +1,51 @@
+@extends('Personne.accueil')
+@section('content')
+    <section id="basic-datatable">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Bienvenue sur Notre site de Covoiturage</h4>
+                    </div>
+                    <div class="card-content">
+                        <div class="card-body card-dashboard">
+                            <p class="card-text">Liste des Trajets</p>
+                                <div class="table-responsive">
+                                    <table class="table zero-configuration">
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Position</th>
+                                                <th>Office</th>
+                                                <th>Age</th>
+                                                <th>Start date</th>
+                                                <th>Salary</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+@section('js')
+    <script src="/app-assets/vendors/js/tables/datatable/pdfmake.min.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/vfs_fonts.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/datatables.min.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/datatables.buttons.min.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/buttons.html5.min.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/buttons.print.min.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js"></script>
+    <script src="/app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js"></script>
+    <script src="/app-assets/js/scripts/datatables/datatable.js"></script>
+@endsection
+@section('css')
+    <link rel="stylesheet" type="text/css" href="/app-assets/vendors/css/tables/datatable/datatables.min.css">
+@endsection
